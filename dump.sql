3308ad72b03cb018121a89448591c836658ca1ca
CarlileC <carlilec@uab.edu>
2024-07-06 17:43:02 -0500

    Added dump.sql. A Starter Database Schema

---
+++ dump.sql
@@ -0,0 +1,128 @@
+-- MySQL dump 10.13  Distrib 8.0.37, for Linux (x86_64)
+--
+-- Host: localhost    Database: CoffeeShop
+-- ------------------------------------------------------
+-- Server version	8.0.37-0ubuntu0.22.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Cart`
+--
+
+DROP TABLE IF EXISTS `Cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Cart` (
+  `UsernameCart` varchar(255) DEFAULT NULL,
+  `CoffeeNameCart` varchar(255) DEFAULT NULL,
+  KEY `Username_idx` (`UsernameCart`),
+  KEY `CoffeeName_idx` (`CoffeeNameCart`),
+  CONSTRAINT `CoffeeNameCart` FOREIGN KEY (`CoffeeNameCart`) REFERENCES `Coffee` (`CoffeeName`),
+  CONSTRAINT `UsernameCart` FOREIGN KEY (`UsernameCart`) REFERENCES `Users` (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cart`
+--
+
+LOCK TABLES `Cart` WRITE;
+/*!40000 ALTER TABLE `Cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Coffee`
+--
+
+DROP TABLE IF EXISTS `Coffee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Coffee` (
+  `CoffeeName` varchar(255) NOT NULL,
+  `Price` int DEFAULT NULL,
+  `id` int DEFAULT NULL,
+  PRIMARY KEY (`CoffeeName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Coffee`
+--
+
+LOCK TABLES `Coffee` WRITE;
+/*!40000 ALTER TABLE `Coffee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Coffee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Favorites`
+--
+
+DROP TABLE IF EXISTS `Favorites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Favorites` (
+  `UsernameFavorite` varchar(255) DEFAULT NULL,
+  `CoffeeNameFavorite` varchar(255) DEFAULT NULL,
+  KEY `Username_idx` (`UsernameFavorite`),
+  KEY `CoffeeName_idx` (`CoffeeNameFavorite`),
+  CONSTRAINT `CoffeeNameFavorite` FOREIGN KEY (`CoffeeNameFavorite`) REFERENCES `Coffee` (`CoffeeName`),
+  CONSTRAINT `UsernameFavorite` FOREIGN KEY (`UsernameFavorite`) REFERENCES `Users` (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Favorites`
+--
+
+LOCK TABLES `Favorites` WRITE;
+/*!40000 ALTER TABLE `Favorites` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Favorites` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Users` (
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `fav_id` int DEFAULT NULL,
+  PRIMARY KEY (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES ('Test User','Test Password',NULL);
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-06 17:37:26
