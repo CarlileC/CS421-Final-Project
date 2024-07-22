3907edb4f3789811f5ea041ec600bdd43b2fdd85
Ethan Martignoni <ethanmartignoni287@gmail.com>
2024-07-22 18:58:37 -0500

    created the product models for coffee, books, and video games. modified the user model to relate to cart model. modified signin/up methods to use validate_on_submit() to submit the forms.

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from coffeeInfo import descriptionChoice
 from utilities import passwordCheck
-from flask_login import LoginManager, UserMixin, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+from flask_migrate import Migrate
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, EqualTo
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
-app.config['SQL_TRAC_MODIFICATIONS'] = False
+app.config['SQL_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = "TEMP KEY"
 db = SQLAlchemy(app)
+Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -26,6 +31,11 @@ class User(UserMixin, db.Model):
     lastName = db.Column(db.Text)
     password = db.Column(db.Text)
     email = db.Column(db.Text, unique = True)
+    cart = db.relationship('Cart', backref='user', uselist=False, lazy=True) 
+    """
+    line 34 creates a relationship between the user and the cart, backref='user' essentially creates a user attribute for the cart which lets a cart object access
+    a user, uselist=False makes it a one-to-one relationship with cart, lazy=True means that cart items won't be loaded unless the cart is accessed
+    """
     
     def __init__(self, firstName, lastName, password, email):
         self.firstName = firstName
@@ -36,14 +46,68 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return f"ID: {self.id} Name: {self.firstName} {self.lastName} Email: {self.email} Password: {self.password}"
     
-# class Coffee(db.Model):
-#     _tablename__="Coffee"
+
+"""
+line 56, line 68, line76 creates a new column that allows item to be attached to a cart id (db.ForeignKey('Cart.id'))
+(if i am understanding it correctly)
+"""    
+class Coffee(db.Model):
+    __tablename__="Coffee"
     
-#     id = db.Column(db.Integer, primary_key = True)
-#     coffeeName = db.Column(db.Text)
-#     favCount = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key = True)
+    coffeeName = db.Column(db.Text, nullable = False)
+    favCount = db.Column(db.Integer)
+    price = db.Column(db.Float, nullable = False)
+    cart_id = db.Column(db.Integer, db.ForeignKey('Cart.id'), nullable=True)
+
+class Book(db.Model):
+    __tablename__="Book"
+
+    id = db.Column(db.Integer, primary_key = True)
+    bookName = db.Column(db.Text, nullable = False)
+    price = db.Column(db.Float, nullable = False)
+    cart_id = db.Column(db.Integer, db.ForeignKey('Cart.id'), nullable=True)
+
+class VideoGame(db.Model):
+    __tablename__="Videogame"
+
+    id = db.Column(db.Integer, primary_key = True)
+    gameName = db.Column(db.Text, nullable = False)
+    price = db.Column(db.Float, nullable = False)
+    cart_id = db.Column(db.Integer, db.ForeignKey('Cart.id'), nullable=True)
     
+
+class Cart(db.Model):
+    __tablename__="Cart"
+
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
+    coffeeItems = db.relationship('Coffee', backref="cart", lazy=True)
+    bookItems = db.relationship('Book', backref='cart', lazy=True)
+    gameItems = db.relationship('VideoGame', backref='cart', lazy=True)
+    """
+    line 84 creates the link between the user and the cart just like in the 'product' models
+    lines 85-87 create the cart attributes for each 
+    """
+
+with app.app_context():
+    db.create_all()
 #END SECTION
+
+"""
+The forms for the signin and signup methods
+"""
+class SignInForm(FlaskForm):
+    email = StringField('Enter Your Email', validators = [InputRequired()])
+    password = PasswordField('Enter Your Password', validators = [InputRequired()])
+    submit = SubmitField('Sign In')
+
+class SignUpForm(FlaskForm):
+    email = StringField('Email', validators = [InputRequired()])
+    password = PasswordField('Password', validators = [InputRequired()])
+    confirmPassword = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password', message='Passwords must be the same')])
+    firstName = StringField('First Name', validators = [InputRequired()])
+    lastName = StringField('Last Name', validators = [InputRequired()])
 
 @login_manager.user_loader
 def get_user(user_id):
@@ -58,34 +122,43 @@ def index():
 #Sign In
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    if request.method == "POST": #User submits the form
-        email = request.form.get('email')
-        password = request.form.get('password')
+    form = SignInForm()
+    if form.validate_on_submit(): #User submits the form
+        email = form.email.data
+        password = form.password.data
         user = db.session.query(User).filter(User.email==email, User.password==password).first() #queries the database, looking if there is a combination in the database
         if user is not None: #Is there a user? If so log them in
             login_user(user)
             return redirect(url_for('secretpage'))
         else: #No? Let them try again and give them an error
-            return render_template('signin.html', errorMessage=Markup("<h2>Incorrect username or password"))
+            return render_template('signin.html', errorMessage=Markup("<h2>Incorrect username or password"), form=form)
 
-    return render_template('signin.html') #User clicks on the nav bar link
+    return render_template('signin.html', form=form) #User clicks on the nav bar link
 
 #Still a little messy
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == "POST": #User submits the form
-        firstName:str = request.form.get('firstName')
-        lastName:str = request.form.get('lastName')
-        email:str = request.form.get('email')
-        password:str = request.form.get('password')
-        confirmPassword:str = request.form.get('confirmPassword')
+    form = SignUpForm()
+    if form.validate_on_submit(): #User submits the form
+        firstName:str = form.firstName.data
+        lastName:str = form.lastName.data
+        email:str = form.email.data
+        password:str = form.password.data
+        confirmPassword:str = form.confirmPassword.data
         
         passwordInfo:list = passwordCheck(password, confirmPassword) #located in utilities, checks if password meets requirements
         
         if passwordInfo[0]: #password met requirements!
             try:
-                newUser:User = User(firstName, lastName, password, email)
+                newUser = User(firstName=firstName, lastName=lastName, password=password, email=email)
                 db.session.add(newUser)
+                db.session.flush()  # Ensure the user ID is available
+                
+                # Create a cart for the new user
+                newCart = Cart(user_id=newUser.id)
+                db.session.add(newCart)
+                
+                # Commit the transaction
                 db.session.commit()
                 return redirect(url_for('signin', errorMessage="Thank you, please sign in")) #redirects them to the sign in page, letting them know it worked
             except IntegrityError:
@@ -93,7 +166,7 @@ def signup():
         else: #password failed, error message located in second element of list.
             return render_template('signup.html', errorMessage=passwordInfo[1])
         
-    return render_template("signup.html") #User clicks on the nav bar link
+    return render_template("signup.html", form=form) #User clicks on the nav bar link
 
 #A fun reference, should be removed later
 @app.route('/secretpage', methods=['GET', 'POST'])
@@ -105,15 +178,21 @@ def signout():
     logout_user()
     return redirect(url_for("index"))
 
-#Renders the coffee list
 @app.route('/cart')
 def cart():
-    coffeeImage = Markup("<img src='https://m.media-amazon.com/images/I/81nV6x2ey4L._AC_UF1000,1000_QL80_.jpg'>")
-    return render_template('cart.html', coffeeImage=coffeeImage)
+    if current_user:
+        return render_template('cart.html')
+    else:
+        return render_template('SecretPage.html')
+
+# renders the cart
 
 @app.route('/CoffeeList')
 def CoffeeList():
     return render_template('CoffeeList.html')
+
+
+#Renders the coffee list
 
 
 #I hate the function I made to clean this up. Is there a better way?
